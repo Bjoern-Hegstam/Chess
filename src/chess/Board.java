b6ca9202c7253c45733201c39ab84a529c48763b
Björn Hegstam <bjorn.hegstam@gmail.com>
2014-02-13 20:56:25 +0100

    Initial commit

---
+++ src/chess/Board.java
@@ -0,0 +1,11 @@
+package chess;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Board {
+    private static final Map<String, PieceType> pieceTypes;
+    static {
+        pieceTypes = new HashMap<>();
+    }
+}
